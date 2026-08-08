4869bc08dbe1abfe63dbe95864ff879395701098
Natalia Omaña <natalia051506@gmail.com>
2026-08-08 16:39:13 -0600

    Configure Node.js Express server and Hello API

--- src/app.js
+++ src/app.js
@@ -2,12 +2,11 @@ const express = require('express');
 const logger = require('./middleware/logger');
 
 const app = express();
-const PORT = process.env.PORT || 3000;
 
 app.use(express.json());
 app.use(logger);
 
-app.get('/', (req, res) => {
+app.get('/api/hello', (req, res) => {
     res.status(200).json({
         message: 'Hello API'
     });
